4a4daf5574ce38477fbfc3858f7bc7b291c25a22
«Andrew» <gepatytich@gmail.com>
2024-06-03 21:09:18 +0700

    added lesson 01

--- src/lessons/lesson_01/CalculatorTest.java
+++ src/lessons/lesson_01/CalculatorTest.java
@@ -13,34 +13,43 @@ public class CalculatorTest {
         System.out.println("_______________________________________________________________________________");
          */
 
-//        if (4 != Calculator.calculation(2, 2, '+')) {
-//            throw new AssertionError("Ошибка в методе");
-//        }
-//
-//        if (2 != Calculator.calculation(4, 2, '-')) {
-//            throw new AssertionError("Ошибка в методе");
-//        }
-//
-//        if (4 != Calculator.calculation(8, 2, '/')) {
-//            throw new AssertionError("Ошибка в методе");
-//        }
-//
-//        if (6 != Calculator.calculation(3, 2, '*')) {
-//            throw new AssertionError("Ошибка в методе");
-//        }
+        if (4 != Calculator.calculation(2, 2, '+')) {
+            throw new AssertionError("Ошибка в методе");
+        }
+
+        if (2 != Calculator.calculation(4, 2, '-')) {
+            throw new AssertionError("Ошибка в методе");
+        }
+
+        if (4 != Calculator.calculation(8, 2, '/')) {
+            throw new AssertionError("Ошибка в методе");
+        }
+
+        if (6 != Calculator.calculation(3, 2, '*')) {
+            throw new AssertionError("Ошибка в методе");
+        }
 
         // Те же проверки с использованием assert
         assert 4 == Calculator.calculation(2, 2, '+');
         assert 2 == Calculator.calculation(4, 2, '-');
         assert 4 == Calculator.calculation(8, 2, '/');
-        assert 9 == Calculator.calculation(3, 2, '*');
+        assert 6 == Calculator.calculation(3, 2, '*');
 
-//        try {
-//            Calculator.calculation(8, 4, '_');
-//        } catch (IllegalStateException e) {
-//            if (!e.getMessage().equals("Unexpected value operator: _")) {
-//                throw new AssertionError("Ошибка в методе");
-//            }
-//        }
+        // Проверка базового функционала с целыми числами, с использованием утверждений AssertJ:
+        assertThat(Calculator.calculation(2, 2, '+')).isEqualTo(4);
+        assertThat(Calculator.calculation(4, 2, '-')).isEqualTo(2);
+        assertThat(Calculator.calculation(8, 2, '/')).isEqualTo(4);
+        assertThat(Calculator.calculation(3, 2, '*')).isEqualTo(6);
+
+        assertThatThrownBy(() -> Calculator.calculation(8, 4, '_')).isInstanceOf(IllegalStateException.class);
+
+
+        try {
+            Calculator.calculation(8, 4, '_');
+        } catch (IllegalStateException e) {
+            if (!e.getMessage().equals("Unexpected value operator: _")) {
+                throw new AssertionError("Ошибка в методе");
+            }
+        }
     }
 }
